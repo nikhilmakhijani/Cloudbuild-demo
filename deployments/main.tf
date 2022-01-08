508510ef5a58b64a69ae43a31f4f9b0892396fc4
Nikhil Makhijani <72851103+nikhilmakhijani@users.noreply.github.com>
2022-01-08 14:17:07 +0530

    changing bucket name

--- deployments/main.tf
+++ deployments/main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "bucket" {
-  name          = "cloudbuild-bucket"
+  name          = "cloudbuild-bucket-demo28"
   location      = "asia"
   force_destroy = true
   project = "ninth-palace-336319"
