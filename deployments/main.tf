c42f5f2fd56a8426bec68e0f54eeb7e8b9116111
Nikhil Makhijani <72851103+nikhilmakhijani@users.noreply.github.com>
2022-01-09 17:01:34 +0530

    updating bucket name to test trigger

--- deployments/main.tf
+++ deployments/main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "bucket" {
-  name          = "cloudbuild-bucketdemo-nikhil"
+  name          = "cloudbuild-bucketdemo-nikhil28"
   location      = "asia"
   force_destroy = true
   project = "ninth-palace-336319"
